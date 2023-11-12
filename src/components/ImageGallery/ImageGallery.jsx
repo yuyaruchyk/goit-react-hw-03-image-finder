fdb0c72097acb17aae909e6b2d29fe7c0c21fff0
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-11-12 20:39:46 +0100

    Loader

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,30 +1,35 @@
 import React, { Component } from 'react';
 import { fetchImages } from 'services/getImages';
 import GalleryItem from 'components/GalleryItem/GalleryItem';
+import Loader from 'components/Loader/Loader';
 
 class ImageGallery extends Component {
-    state = {
-        images: null,
-    };
+  state = {
+    images: null,
+    isLoading: false,
+  };
 
-    async componentDidUpdate(prevProps) {
-        if (prevProps.searchText !== this.props.searchText) {
-            try {
-                const images = await fetchImages(this.props.searchText);
+  async componentDidUpdate(prevProps) {
+    if (prevProps.searchText !== this.props.searchText) {
+      try {
+        this.setState({ isLoading: true });
+        const images = await fetchImages(this.props.searchText);
 
-        
-                this.setState({ images });
-            } catch (error) {
-                console.error('Error fetching images:', error.message);
-            }
-        }
+        this.setState({ images });
+      } catch (error) {
+        console.error('Error fetching images:', error.message);
+      } finally {
+        this.setState({ isLoading: false });
+      }
     }
+  }
 
   render() {
-    const { images } = this.state;
+    const { images, isLoading } = this.state;
 
     return (
       <div>
+        {isLoading && <Loader/>}
         {images && (
           <ul>
             {images.map((image) => (
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,16 @@
+
+import { Blocks } from  'react-loader-spinner'
+
+
+export const Loader = () => {
+    <Blocks
+  visible={true}
+  height="80"
+  width="80"
+  ariaLabel="blocks-loading"
+  wrapperStyle={{}}
+  wrapperClass="blocks-wrapper"
+/>
+}
+
+export default Loader;
