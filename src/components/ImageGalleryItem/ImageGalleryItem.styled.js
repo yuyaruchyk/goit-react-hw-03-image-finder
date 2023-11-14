063a6d4472183429658cc1189d8a7cc198a6871d
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-11-14 20:55:06 +0100

    upd

--- src/components/ImageGalleryItem/ImageGalleryItem.styled.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.styled.js
@@ -7,7 +7,7 @@ export const GalleryImage = styled.li`
 `;
 
 export const Image = styled.img`
-  height: 260px;
+  
   object-fit: cover;
   transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1);
   &:hover {
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -10,24 +10,6 @@ const customStyles = {
     marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
   },
-
-  overlay: {
-    position: `fixed`,
-    top: `0`,
-    left: `0`,
-    width: `100vw`,
-    height: `100vh`,
-    display: `flex`,
-    justifyContent: `center`,
-    alignItems: `center`,
-    backgroundColor: `rgba(0, 0, 0, 0.8)`,
-    zIndex: `1200`,
-  },
-
-  modal: {
-    maxWidth: `calc(100vw - 48px)`,
-    maxHeight: `calc(100vh - 24px)`,
-  },
 };
 
 Modal.setAppElement('#root');
